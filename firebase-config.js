0426902fd28f2a13c4a4def34d77dc98ecf52a4c
benwieler-commits <benwieler@gmail.com>
2025-11-05 22:54:02 -0500

    Player App: Luxy folio + theme color from JSON + tags/roller + Firebase listener

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,3 @@
-// Firebase (Player)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-database.js";
-
 export const firebaseConfig = {
   apiKey: "AIzaSyDOeJQjTm0xuFDAhhLaWP6d_kK_hNwRY58",
   authDomain: "queerz-mc-live.firebaseapp.com",
@@ -11,6 +7,3 @@ export const firebaseConfig = {
   messagingSenderId: "155846709409",
   appId: "1:155846709409:web:8c12204dc7d502586a20e0"
 };
-
-export const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
